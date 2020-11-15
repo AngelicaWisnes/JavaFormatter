e3159a3b6459116274cc7480229ed13b8670ef94
RogerWisnes <rogerwisnes@gmail.com>
2020-11-15 14:22:04 +0100

    Create GlobalQuickConfig.java

---
+++ src/main/java/no/uib/inf225/java_formatter/GlobalQuickConfig.java
@@ -0,0 +1,31 @@
+package no.uib.inf225.java_formatter;
+
+import no.uib.inf225.java_formatter.util.Ansi;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Utility class for easy access to final variables
+ */
+public final class GlobalQuickConfig {
+
+
+    private static final Path DIRECTORY_ROOT = Paths.get(System.getProperty("user.dir") + "\\src");
+
+    private static final List<String> LEGAL_FILE_EXTENSION = Arrays.asList("java", "class");
+
+    private GlobalQuickConfig() {
+    }
+
+    public static Path getDirectoryRoot() {
+        return DIRECTORY_ROOT;
+    }
+
+    public static boolean isLegalFileExtension(String fileExtension) {
+        return LEGAL_FILE_EXTENSION.contains(fileExtension);
+    }
+
+}
