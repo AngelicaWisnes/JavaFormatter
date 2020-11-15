ef44e29ff6e0b505acba265e3026e14dfb11e402
RogerWisnes <rogerwisnes@gmail.com>
2020-11-15 14:22:15 +0100

    Create Main.java

---
+++ src/main/java/no/uib/inf225/java_formatter/Main.java
@@ -0,0 +1,23 @@
+package no.uib.inf225.java_formatter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+
+
+public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private static final DirectoryScanner directoryScanner = new DirectoryScanner();
+    private static final Path directoryRoot = GlobalQuickConfig.getDirectoryRoot();
+
+
+    public static void main(String[] args) {
+        LOGGER.info("Using directory root {}", directoryRoot);
+
+        directoryScanner.traverseDirectory(directoryRoot, 0);
+        LOGGER.info("Finished scanning the directory: \n{}", directoryScanner.getDirectoryImage());
+    }
+
+}
