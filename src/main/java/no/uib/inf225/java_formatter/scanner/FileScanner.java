8fc837668ed0a7e3238091830a27a8ad3aeb7890
RogerWisnes <rogerwisnes@gmail.com>
2020-11-16 14:36:24 +0100

    Added FileScanner and implementet scanning and handling method

---
+++ src/main/java/no/uib/inf225/java_formatter/scanner/FileScanner.java
@@ -0,0 +1,43 @@
+package no.uib.inf225.java_formatter.scanner;
+
+import no.uib.inf225.java_formatter.Java9Lexer;
+import no.uib.inf225.java_formatter.Java9Parser;
+import no.uib.inf225.java_formatter.JavaListener;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.*;
+
+public class FileScanner {
+
+    public void scan(Path entry) {
+        String fileName = entry.getFileName().toString();
+        if (fileName.equals("BookClub.java")) {
+            try {
+                File myObj = new File(entry.toString());
+                Scanner myReader = new Scanner(myObj);
+
+                while (myReader.hasNextLine()) handle(myReader.nextLine());
+
+                myReader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void handle(String data) throws Exception {
+        Java9Lexer java9Lexer = new Java9Lexer(CharStreams.fromString(data));
+        CommonTokenStream tokens = new CommonTokenStream(java9Lexer);
+        Java9Parser java9Parser = new Java9Parser(tokens);
+        ParseTree tree = java9Parser.compilationUnit();
+        ParseTreeWalker walker = new ParseTreeWalker();
+
+        JavaListener javaListener = new JavaListener();
+        walker.walk(javaListener, tree);
+    }
+}
