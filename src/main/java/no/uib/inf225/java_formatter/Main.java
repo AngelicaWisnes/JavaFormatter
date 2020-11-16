7de983396ea3de4f3939b7040e69d00624584257
RogerWisnes <rogerwisnes@gmail.com>
2020-11-16 14:46:34 +0100

    Added method for analysing the single-run-log

--- src/main/java/no/uib/inf225/java_formatter/Main.java
+++ src/main/java/no/uib/inf225/java_formatter/Main.java
@@ -4,21 +4,58 @@ import no.uib.inf225.java_formatter.scanner.DirectoryScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.nio.file.Path;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-    private static final DirectoryScanner directoryScanner = new DirectoryScanner();
-    private static final Path directoryRoot = GlobalQuickConfig.getDirectoryRoot();
+    private static final DirectoryScanner DIRECTORY_SCANNER = new DirectoryScanner();
+    private static final Path DIRECTORY_ROOT = GlobalQuickConfig.getDirectoryRoot();
 
 
     public static void main(String[] args) {
-        LOGGER.info("Using directory root {}", directoryRoot);
+        LOGGER.info("Using directory root {}", DIRECTORY_ROOT);
 
-        directoryScanner.traverseDirectory(directoryRoot, 0);
-        LOGGER.info("Finished scanning the directory: \n{}", directoryScanner.getDirectoryImage());
+        DIRECTORY_SCANNER.traverseDirectory(Path.of(DIRECTORY_ROOT + "\\src"), 0);
+        LOGGER.info("Finished scanning the directory: \n{}", DIRECTORY_SCANNER.getDirectoryImage());
+
+        checkLogForUsedMethods();
     }
 
+
+    public static void checkLogForUsedMethods() {
+        try {
+            String fileName = (DIRECTORY_ROOT + "\\log\\singleRunLogFile.log");
+            File file = new File(fileName);
+            Scanner sc = new Scanner(file);
+
+            // Define the regex-pattern
+            String regex = "(?<=((Method: enter)|(Method: exit)))([a-zA-Z0-9_]*)";
+            Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+            // Add all regex-matches to list
+            List<String> methods = new ArrayList<>();
+            while (sc.hasNextLine()) {
+                Matcher matcher = pattern.matcher(sc.nextLine());
+                if (matcher.find()) methods.add(matcher.group());
+            }
+
+            // Clean up list (Delete duplicates, and sort)
+            Set<String> set = new HashSet<>(methods);
+            methods.clear();
+            methods.addAll(set);
+            Collections.sort(methods);
+
+            for (String s : methods) System.out.println(s);
+
+            sc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
