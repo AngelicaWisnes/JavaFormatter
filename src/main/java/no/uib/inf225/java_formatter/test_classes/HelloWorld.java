a286883a49c9d35d4a72c52ebebcc09b0dd66a75
RogerWisnes <rogerwisnes@gmail.com>
2020-11-19 00:12:44 +0100

    Added a new test-class HelloWorld.java

---
+++ src/main/java/no/uib/inf225/java_formatter/test_classes/HelloWorld.java
@@ -0,0 +1,9 @@
+package no.uib.inf225.java_formatter.test_classes;
+
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
