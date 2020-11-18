4a849f3dabfe079c6f806feb7b12b272c70314ba
RogerWisnes <rogerwisnes@gmail.com>
2020-11-19 00:18:32 +0100

    Added prettyprinting and visitor

--- src/main/java/no/uib/inf225/java_formatter/scanner/FileScanner.java
+++ src/main/java/no/uib/inf225/java_formatter/scanner/FileScanner.java
@@ -1,37 +1,59 @@
 package no.uib.inf225.java_formatter.scanner;
 
+import no.uib.inf225.java_formatter.Java9BaseVisitor;
 import no.uib.inf225.java_formatter.Java9Lexer;
 import no.uib.inf225.java_formatter.Java9Parser;
-import no.uib.inf225.java_formatter.JavaListener;
+import no.uib.inf225.java_formatter.JavaVisitor;
+import no.uib.inf225.java_formatter.util.TreePrinter;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileScanner {
 
-    public void scan(Path entry) {
-        String fileName = entry.getFileName().toString();
-        if (fileName.equals("BookClub.java")) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileScanner.class);
+
+    public void scan(Path file) {
+        String fileName = file.getFileName().toString();
+
+        if (fileName.equals("HelloWorld.java")) {
+            LOGGER.info("Scanner initiated with file: {}", fileName);
             try {
-                handle(new String(Files.readAllBytes(entry)));
+                handle(file);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public void handle(String data) throws Exception {
-        Java9Lexer java9Lexer = new Java9Lexer(CharStreams.fromString(data));
-        CommonTokenStream tokens = new CommonTokenStream(java9Lexer);
-        Java9Parser java9Parser = new Java9Parser(tokens);
-        ParseTree tree = java9Parser.compilationUnit();
-        ParseTreeWalker walker = new ParseTreeWalker();
+    public void handle(Path file) throws Exception {
+        LOGGER.info("Started handling of file: {}", file.getFileName());
 
-        JavaListener javaListener = new JavaListener();
-        walker.walk(javaListener, tree);
+        String data = new String(Files.readAllBytes(file));
+        Java9Lexer lexer = new Java9Lexer(CharStreams.fromString(data));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        Java9Parser parser = new Java9Parser(tokens);
+        ParseTree tree = parser.compilationUnit();
+
+        //ParseTreeWalker walker = new ParseTreeWalker();
+
+        //JavaListener javaListener = new JavaListener();
+        //walker.walk(javaListener, tree);
+
+        LOGGER.info("STARTING VISITOR");
+        JavaVisitor visitor = new JavaVisitor();
+        visitor.visit(tree);
+        LOGGER.info("DONE VISITING");
+
+
+
+        TreePrinter.prettyPrint(tree, Arrays.asList(parser.getRuleNames()));
     }
 }
