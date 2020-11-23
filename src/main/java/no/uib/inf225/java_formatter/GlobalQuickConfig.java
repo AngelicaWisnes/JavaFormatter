f96058a0f1242e3e15b9d3ebdb53f800c33ec2d6
RogerWisnes <rogerwisnes@gmail.com>
2020-11-23 21:11:40 +0100

    Added default Formatting-style to globalconfig

--- src/main/java/no/uib/inf225/java_formatter/GlobalQuickConfig.java
+++ src/main/java/no/uib/inf225/java_formatter/GlobalQuickConfig.java
@@ -1,6 +1,8 @@
 package no.uib.inf225.java_formatter;
 
-import no.uib.inf225.java_formatter.util.Ansi;
+import no.uib.inf225.java_formatter.rules.DefaultStyle;
+import no.uib.inf225.java_formatter.rules.JavaFormatter;
+import no.uib.inf225.java_formatter.rules.IStyle;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,10 +14,12 @@ import java.util.List;
  */
 public final class GlobalQuickConfig {
 
-
+    // Directory and files
     private static final Path DIRECTORY_ROOT = Paths.get(System.getProperty("user.dir"));
-
     private static final List<String> LEGAL_FILE_EXTENSION = Arrays.asList("java", "class");
+
+    // Formatting styles
+    private static final IStyle FORMATTING_STYLE = new DefaultStyle();
 
     private GlobalQuickConfig() {
     }
@@ -28,4 +32,7 @@ public final class GlobalQuickConfig {
         return LEGAL_FILE_EXTENSION.contains(fileExtension);
     }
 
+    public static IStyle getFormattingStyle() {
+        return FORMATTING_STYLE;
+    }
 }
