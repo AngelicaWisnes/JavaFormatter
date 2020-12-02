ed5dd0026e93563366723c9f4d4453bfb03bc28b
RogerWisnes <rogerwisnes@gmail.com>
2020-12-02 14:27:51 +0100

    Extended test-class

--- src/main/java/no/uib/inf225/java_formatter/test_classes/TestClass1.java
+++ src/main/java/no/uib/inf225/java_formatter/test_classes/TestClass1.java
@@ -1,6 +1,46 @@
 package no.uib.inf225.java_formatter.test_classes;
 
+import java.util.ArrayList;
+
 public class TestClass1 {
+
+
+    private static ArrayList<Integer>[] hasOfInterest;
+    private static int[] matched;
+    private static boolean[] visited;
+
+    public static void main(String[] args) {
+        Kattio io = new Kattio(System.in);
+        int a = -1;
+        int b = 1 + 1;
+        int c = 1 - 1;
+        int d = 2 + (3 / 4 + 1);
+        int N = io.getInt();
+        int M = io.getInt();
+        hasOfInterest = new ArrayList[N];
+        matched = new int[N];
+        for (int i = 0; i < N; i++) hasOfInterest[i] = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            hasOfInterest[io.getInt()].add(io.getInt());
+        }
+        for (int i = 0; i < N; i++)
+            matched[i] = -1;
+        io.println(matchPossible(N) ? "YES" : "NO");
+        io.flush();
+        io.close();
+    }
+
+    private static boolean matchPossible(int person) {
+        for (int book : hasOfInterest[person]) {
+            if (visited[book]) continue;
+            visited[book] = true;
+            if (matched[book] == -1 || matchPossible(matched[book])) {
+                matched[book] = person;
+                return true;
+            }
+        }
+        return false;
+    }
 
     private static final String STR = "test";
     private int a;
@@ -12,6 +52,10 @@ public class TestClass1 {
         this.a = a;
         this.b = b;
         this.c = c;
+    }
+
+    public static void setVisited(boolean[] visited) {
+        TestClass1.visited = visited;
     }
 
     public String getSTR() {
